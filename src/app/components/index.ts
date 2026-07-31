83200a2d69fbc619347a010b77c5296505865686
kaio <kaitozhiroki@gmail.com>
2026-07-31 20:00:16 -0300

    feat: creating index for components

---
+++ src/app/components/index.ts
@@ -0,0 +1,8 @@
+export { default as NavBar } from "./nav-bar";
+export { default as WhatsAppSVG } from "./whatsapp-svg";
+export { default as About } from "./second-section/about";
+export { default as Cards } from "./second-section/cards";
+export { default as Button } from "./button";
+export { default as Help } from "./third-section/help";
+export { default as SectionDivider } from "./section-divider";
+export { default as CustomCards } from "./third-section/custom-cards";
